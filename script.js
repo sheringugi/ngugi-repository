9c5aa11a99b58a631598d67e3fbc59250e66e843
sheringugi <sheri.ngugi17@gmail.com>
2023-03-10 12:18:56 +0300

    add script.js, connect to index.html

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ const h2 = document.createElement("h2");
 h2.textContent ="This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
 console.log(h2)
+const h3 = document.createElement("h3");
+h3.textContent ="Visual effects made by CSS";
+document.querySelector("body").appendChild(h3);
+console.log(h3)
