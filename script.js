99491422ab68be1a31045eabd630395c2e4777e9
sheringugi <sheri.ngugi17@gmail.com>
2023-03-10 12:11:34 +0300

    addition

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent.Content ="This content added by JavaScript";
+h2.textContent ="This content added by JavaScript";
 document.querySelector("body").appendChild(h2);
+console.log(h2)
